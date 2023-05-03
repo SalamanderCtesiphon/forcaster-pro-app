0b7148cb9884c7d8853c70d9e2d7268de114503a
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-03 12:00:40 -0500

    styling the body

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 function Body() {
+
+
   return (
-    <div className='body'>Body</div>
+    <div className='body'>
+      <form className='search-form'>
+        <h3>Seach City or Zip Code:  </h3>
+        <input id='input'></input>
+        <button id='search-btn'>submit</button>
+      </form>
+      <div className='display'>
+        Hi
+      </div>
+    </div>
   )
 }
 
