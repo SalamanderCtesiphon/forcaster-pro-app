28ff54ccb68be7ee47e86abff74d7d6e63211cc1
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-03 11:32:21 -0500

    added current year and copywrite sign to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
 function Footer() {
+  const currentYear = new Date().getFullYear()
   return (
-    <div>Footer</div>
+    <div>Weather App &#169; {currentYear}</div>
   )
 }
 
