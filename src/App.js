f96f7676a10cbeef307f2ff22d426d07399e632e
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-03 12:38:00 -0500

    fixed a small bug with the css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Body from './components/Body';
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Body />
-      <Footer />
+    <div className='App'>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
