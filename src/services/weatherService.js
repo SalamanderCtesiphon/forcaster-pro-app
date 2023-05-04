8bc5b5ca45f2b94a4515decd0339c156d1047a19
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-04 16:03:24 -0500

    my freaking api key wont work and the new ones that i have generated arent working either. such fun

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,44 +1,96 @@
+import { DateTime } from "luxon"
+
 const API_KEY = "5e0ec4efef4e3b90d91ea7723d1db6af"
 const BASE_URL = "https://api.openweathermap.org/data/2.5"
 
-const getWeatherData = (infoType, searchParams) => {
-  const url = new URL(BASE_URL + "/" + infoType)
-  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY})
+const getWeatherData = async(infoType, searchParams) => {
+  const url = new URL(BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
 
-  return fetch(url).then((res) => res.json())
-}
+  return fetch(url).then((res) => res.json());
+};
 
-const formatCurrentWeather =(data) => {
+const formatCurrentWeather = (data) => {
   const {
-    coord: {lat, lon},
-    main: {temp, feels_like, temp_min, temp_max, humidity},
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
     name,
     dt,
-    sys: {country, sunrise, sunset},
+    sys: { country, sunrise, sunset },
     weather,
-    wind: {speed}
-  } = data
+    wind: { speed },
+  } = data;
 
-  const {main: details, icon} = weather[0]
+  const { main: details, icon } = weather[0];
 
-  return {lat, lon, temp, feels_like, temp_min, temp_max, humidity, name, dt, country, sunrise, sunset, details, icon, speed}
-}
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    details,
+    icon,
+    speed,
+  };
+};
 
 const formatForecastWeather = (data) => {
-  let { timezone, daily, hourly} = data
-  daily = daily.slice(1,6).map(())
-}
+  console.log(data)
+  let { timezone, daily, hourly } = data;
+  daily = daily.slice(1, 6).map((d) => {
+    return {
+      title: formatToLocalTime(d.dt, timezone, "ccc"),
+      temp: d.temp.day,
+      icon: d.weather[0].icon,
+    };
+  });
 
-const getFormattedWeatherData = async(searchParams) => {
-  const formattedCurrentWeather = await getWeatherData ('weather', searchParams).then(formatCurrentWeather)
+  hourly = hourly.slice(1, 6).map((d) => {
+    return {
+      title: formatToLocalTime(d.dt, timezone, "hh:mm a"),
+      temp: d.temp,
+      icon: d.weather[0].icon,
+    };
+  });
 
-  const {lat, lon} = formatCurrentWeather
+  return { timezone, daily, hourly };
+};
 
-  const formattedForcastWeather = await getWeatherData('onecall', {
-    lat, lon, exclude: 'current,minutely,alerts', units: searchParams
-  }).then(formatForecastWeather)
+const getFormattedWeatherData = async (searchParams) => {
+  const formattedCurrentWeather = await getWeatherData(
+    "weather",
+    searchParams
+  ).then(formatCurrentWeather);
 
-  return formattedCurrentWeather
-}
+  const { lat, lon } = formattedCurrentWeather;
 
-export default getFormattedWeatherData
+  const formattedForecastWeather = await getWeatherData("onecall", {
+    lat,
+    lon,
+    exclude: "current,minutely,alerts",
+    units: searchParams.units,
+  }).then(formatForecastWeather);
+
+  return { ...formattedCurrentWeather, ...formattedForecastWeather };
+};
+
+const formatToLocalTime = (
+  secs,
+  zone,
+  format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a"
+) => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
+
+const iconUrlFromCode = (code) =>
+  `http://openweathermap.org/img/wn/${code}@2x.png`;
+
+export default getFormattedWeatherData;
+
+export { formatToLocalTime, iconUrlFromCode };
