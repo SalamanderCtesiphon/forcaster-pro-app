1f70f9c0244d91a9e35c698037bedd6105b6eafc
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-04 11:19:39 -0500

    added the forcast component to the main app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import TopButtons from './components/TopButtons';
 import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
+import Forcast from './components/Forcast';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
 
       <TimeAndLocation />
       <TemperatureAndDetails />
+      <Forcast />
     </div>
   );
 }
