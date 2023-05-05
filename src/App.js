f1733d14f8635d5f7381e5ee81f7a442181d80a8
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-05 11:08:36 -0500

    bug hunting

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import getFormattedWeatherData from './services/weatherService';
 
 function App() {
   const fetchWeather = async() => {
-    const data = await getFormattedWeatherData({q: 'london'})
+    const data = await getFormattedWeatherData('weather', {q: 'london'})
     console.log(data)
   }
 
